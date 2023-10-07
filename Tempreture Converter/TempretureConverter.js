c42495d48fa8163221c772ab915647b719dc1c30
Abhik Saran <110584674+AbhikDevX@users.noreply.github.com>
2023-10-07 15:35:38 +0530

    TempretureConverter.js

--- Tempreture Converter/TempretureConverter.js
+++ Tempreture Converter/TempretureConverter.js
@@ -1,1 +1,21 @@
-
+document.getElementById("btn").addEventListener("click", function () {
+    const temperature = parseFloat(document.getElementById("Temperature").value);
+    const unit = document.getElementById("units").value;
+    let convertedTemperature;
+  
+    if (isNaN(temperature)) {
+      document.getElementById("result").textContent = "Please enter a valid number.";
+      return;
+    }
+  
+    if (unit === "Celsius") {
+      convertedTemperature = (temperature * 9/5) + 32;
+      document.getElementById("result").textContent = `Converted Temperature: ${convertedTemperature.toFixed(2)} °F`;
+    } else if (unit === "Fahrenheit") {
+      convertedTemperature = (temperature - 32) * 5/9;
+      document.getElementById("result").textContent = `Converted Temperature: ${convertedTemperature.toFixed(2)} °C`;
+    } else if (unit === "Kelvin") {
+      convertedTemperature = temperature + 273.15;
+      document.getElementById("result").textContent = `Converted Temperature: ${convertedTemperature.toFixed(2)} K`;
+    }
+  });
